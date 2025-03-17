0f312e610e67614292cfe8ad5807b07be258d7dc
Dhruv Sood <dhruv.sood@sourcefuse.com>
2025-03-17 17:46:24 -0400

    Init Spring backend and React frontend

---
+++ cpa-backend/cpa-api/src/main/java/com/skillstorm/cpa/CpaApiApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.cpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpaApiApplication.class, args);
+	}
+
+}
